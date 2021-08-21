8620814f7a89d4beedd1e16993230b24c8fa575a
Leesa Ward <leesa@doubleedesign.com.au>
2021-08-21 20:23:31 +1000

    Whoops, missed the Sign component styling in last commit

--- src/Components/Sign/Sign.js
+++ src/Components/Sign/Sign.js
@@ -5,8 +5,8 @@ import React from "react";
 function Sign(props) {
 
     return (
-        <div className="signs__item" key={props.title}>
-            <div className="signs__item__inner">
+        <div className="sign" key={props.title}>
+            <div className="sign__inner">
                 <h2>{props.title}</h2>
                 <img src={props.image} alt={props.title} height='200'/>
                 <Speech text={props.title} textAsButton />
