f764c7beb6cb689706e2977c3ea589dfc8a3d42a
Mitchie Thomson <mgtho@deakin.edu.au>
2021-08-22 01:41:30 +1000

    About styling

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,30 +5,37 @@ import './_About.scss';
 function About () {
   return (
       <Fragment>
+      <div className="container">
         <PageHeader title="About"/>
-        <div>
-          <iframe width='300' height='200' src='https://www.youtube.com/embed/S3ZX1KV7IHY' title='YouTube video player'
-                  frameBorder='0'
-                  allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-                  allowFullScreen/>
-          <p>
-            A web app created to help children who struggle to communicate by providing an AAC application to reinforce the
-            Keyword Sign gestures reinforced aurally.
-          </p>
-          <br/>
-          <p>
-            Thus, allowing children to practice their Keyword Sign and giving Speech Pathologists reinforcement learning tools
-            during their sessions.
-          </p>
-          <p>
-            Its not to teach the kids key word sign but help them verbalise what they are thinking.
-          </p>
-          <br/>
-          <p>
-            If a 2 year old knows Key Word Sign but a baby sitter come, then it can be used to help the kid verbalise what
-            they are thinking since learning gestures is easier for a child.
-          </p>
-        </div>
+          <div className="about">
+
+            <div className="about_block">
+              <h2>What is keyword sign?</h2>
+              <div className="about_block_text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dolor turpis, aliquet a nulla et, volutpat egestas urna. Cras a enim non dolor vehicula auctor nec eget felis. Quisque quis metus ac erat scelerisque malesuada. 
+              </div>
+            </div>
+            <div className="about_block">
+              <h2>Who is this for?</h2>
+              <div className="about_block_text">
+                #TODO: list styling? Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dolor turpis, aliquet a nulla et, volutpat egestas urna. Cras a enim non dolor vehicula auctor nec eget felis. 
+              </div> 
+            </div>
+
+            <div className="resources">
+              <h2>Further resources</h2>
+              <div className="resources_block">
+                <div className="video">
+                  <iframe src='https://www.youtube.com/embed/S3ZX1KV7IHY' title='YouTube video player' frameBorder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen/>
+                </div>
+                  <div className="resources_block_text">
+                    #TODO: list styling, find further links or make video fullsize
+                  </div>
+                </div>
+              </div>
+          </div>
+          
+      </div>
       </Fragment>
   )
 }
