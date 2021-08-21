fb10be2d6bb60148f87927c7c4f482c815f785aa
PandaPlaysAll <josh.piper@live.com.au>
2021-08-21 12:57:02 +1000

    Reading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import firebase from "./firebase";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const [signs, setSigns] = useState([]);
+const [loading, setLoading] = useState(false);
+
+const ref = firebase.firestore().collection("keywords");
+
+const getKeywords = () => {
+  setLoading(true);
+  ref.onSnapshot((querySnapshot) => {
+    const items = [];
+    querySnapshot.forEach((doc) => {
+      items.push(doc.data());
+    });
+    setSigns(items);
+    setLoading(false);
+  })
+}
+
+useEffect(() => {
+  getKeywords();
+}, []);
+
+if (loading) {
+  return <h1>Loading...</h1>;
+}
+
+return (
+  <div>
+    <h1>Signs</h1>
+    {signs.map((sign) => (
+      <div key={sign.title}>
+        <h2>{sign.title}</h2>
+        <img src={sign.image} alt={sign.title} height="200"/>
+      </div>
+    ))}
+  </div>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/app"
+import "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCdAVqWKh-EzE7BVtFeCPfu6KPOaSnHbx8",
+    authDomain: "key-word-sign-db.firebaseapp.com",
+    projectId: "key-word-sign-db",
+    storageBucket: "key-word-sign-db.appspot.com",
+    messagingSenderId: "908087048988",
+    appId: "1:908087048988:web:92dbb19dc77473d85258d9",
+    measurementId: "G-YJK04R9PXK"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
