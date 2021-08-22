4358411376b03c05042c80b39d4cbae7f69eea29
Mitchie Thomson <mgtho@deakin.edu.au>
2021-08-22 10:50:52 +1000

    Fixing typos

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -18,7 +18,7 @@ const About = () => {
           <div className='about_block'>
             <h2>Who is this for?</h2>
             <div className='about_block_text'>
-              The application is design as an Augmentative and Alternative Communication program to help children and adults communicate with eachother. For example, most adults do not know Key Word Sign nor AusLan, thus, they can open the application for clarification.
+              This application is an Augmentative and Alternative Communication program to help children and adults communicate with each other. Many adults may not know Key Word Sign nor AusLan, in which case they can open the application for clarification.
             </div>
           </div>
 
@@ -29,7 +29,7 @@ const About = () => {
                 <iframe src='https://www.youtube.com/embed/S3ZX1KV7IHY' title='YouTube video player' frameBorder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen />
               </div>
               <div className='resources_block_text'>
-                Libby Brownlie a speech pathologist brought Key Word Sign to Australia. Learn more by watching this video!
+                Libby Brownlie, a speech pathologist, brought Key Word Sign to Australia. Learn more by watching this video!
               </div>
             </div>
           </div>
